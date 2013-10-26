0c44a9e1dd1264ae07f7dd6b1572bc7f008d9a91
Henry Rose <henryrose@gmail.com>
2013-10-26 11:07:14 -0700

    Adds Gruntfile

---
+++ GruntFile.js
@@ -0,0 +1,26 @@
+/** Task Runner **/
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'), 
+    jshint: {
+        // define the files to lint
+        files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+        // configure JSHint (documented at http://www.jshint.com/docs/)
+        options: {
+          // more options here if you want to override JSHint defaults
+            globals: {
+                jQuery: true,
+                console: true,
+                module: true
+            }
+        }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+   grunt.registerTask('default', ['jshint']);
+
+};
